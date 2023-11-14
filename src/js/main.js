115489cab94b17c2fe9c66c7f2c21d51dc89dfd1
Tomo <94550718+Tomo1151@users.noreply.github.com>
2023-11-14 23:45:04 +0900

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,5 @@
 import GameManager from "./game_manager_.js";
-import { Disk, Board } from "./object.js";
-import Player from "./player.js";
-import Enemy from "./enemy.js";
-
 console.log("Hello, world!");
-
-// const board = new Board(8, 8);
-// const player = new Player(Disk.WHITE);
-// const enemy = new Enemy(Disk.BLACK);
-// const gm = new GameManager([player, enemy]);
 
 const game_manager = new GameManager();
 game_manager.run();
