2cc467705101c41da6d41798fb285d3de1e842b4
Tomo <94550718+Tomo1151@users.noreply.github.com>
2023-11-18 14:39:58 +0900

    result screen & order ui

--- src/js/section/ResultSection/result_section.js
+++ src/js/section/ResultSection/result_section.js
@@ -5,16 +5,30 @@ import Section from "../section.js";
 import * as Event from "../../event.js";
 
 export default class ResultSection extends Section {
-
-	constructor(game_manager, renderer_manager, scene) {
+	#result;
+	constructor(game_manager, renderer_manager, scene, result) {
 		super(game_manager, renderer_manager, scene);
+		this.#result = result;
 	}
 
 	run() {}
 	init() {
-		const geometry = new THREE.BoxGeometry(5, 5, 5);
-		const material = new THREE.MeshStandardMaterial({color: 0x0000FF});
-		const box = new THREE.Mesh(geometry, material);
-		this.scene.add(box);
+		this.renderer_manager.controls.enabled = false;
+		this.drawResultScreen();
+	}
+
+	drawResultScreen() {
+		let container = document.getElementById('result_screen');
+		let dom_score = document.getElementById('score');
+		let dom_order_black = document.getElementById('order_black');
+		let dom_order_white = document.getElementById('order_white');
+
+		container.style.display = 'block';
+
+		console.log(this.#result);
+
+		dom_order_white.innerText = this.#result.white;
+		dom_order_black.innerText = this.#result.black;
+		dom_score.innerText = -1;
 	}
 }
