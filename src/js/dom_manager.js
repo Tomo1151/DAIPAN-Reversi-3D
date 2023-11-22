c997e369eb742a196577a05ae63ddcf8d4a44b05
Tomo <94550718+Tomo1151@users.noreply.github.com>
2023-11-22 15:32:15 +0900

    game reset

--- src/js/dom_manager.js
+++ src/js/dom_manager.js
@@ -1,4 +1,5 @@
 import * as Event from "./event.js"
+import { Disk } from "./object.js";
 import GameManager from "./game_manager.js";
 import GameSection from "./section/GameSection/game_section.js";
 
@@ -25,50 +26,79 @@ export default class DOMManager {
 		this.#restart_button = document.getElementById('restart_button');
 		// this.#title_screen_dom = document.getElementById();
 
+		this.#game_manager.addEventListener('game_restart', () => {
+			console.log('GAME RESTART');
+			this.hide(this.#order_dom);
+			this.hide(this.#ingame_buttons);
+			this.hide(this.#result_screen_dom);
+			this.show(this.#title_screen_dom);
+		});
+	}
+
+	addDOMEventListener() {
 		// def Game Events
 		document.getElementById('start_button').addEventListener('click', () => {
 			// Setting DOMs
 			console.log("* send: game_start");console.log("");
-			let div = document.getElementById('title_screen');
-			div.style.display = 'none';
-			let action_buttons = document.getElementById('action_button');
-			action_buttons.style.display = 'block';
-
-			/*
-			 * GameSection
-			 */
-
-			action_buttons.children[0].addEventListener('click', () => {
-				if (this.#game_manager.current_turn != this.#game_manager.player.order) return;
-				this.#game_manager.current_section.mode = GameSection.MODE_PUT;
-				console.log(this.#game_manager.current_section.mode)
-			});
-			action_buttons.children[1].addEventListener('click', () => {
-				if (this.#game_manager.current_turn != this.#game_manager.player.order || this.#game_manager.checkTable(this.#game_manager.player.order)) return;
-				this.#game_manager.dispatchEvent(new Event.PutPassEvent(this.#game_manager.player.order));
-				document.getElementById('pass_button').classList.add('disabled');
-			});
-			action_buttons.children[2].addEventListener('click', () => {
-				if (this.#game_manager.current_turn != this.#game_manager.player.order) return;
-				this.#game_manager.current_section.mode = GameSection.MODE_PUT;
-				// this.#current_section.mode = GameSection.MODE_BANG;
-			});
-
-			/*
-			 * ResultSection
-			 */
-			// this.#reset_button.addEventListener('click', () => {
-			// 	this.#game_manager.dispatchEvent(new Event.GameRestartEvent());
-			// });
-
-			this.#game_manager.addEventListener('game_restart', () => {
-				console.log('GAME RESTART');
-				this.#result_screen_dom.style.display = 'none';
-				this.#title_screen_dom.style.display = 'block';
-			});
+			this.hide(this.#title_screen_dom);
+			this.show(this.#ingame_buttons);
+			this.show(this.#order_dom, true);
 
 			this.#game_manager.dispatchEvent(new Event.GameStartEvent());
 		});
+
+		/*
+		 * GameSection
+		 */
+		this.#ingame_buttons.children[0].addEventListener('click', () => {
+			if (this.#game_manager.current_turn != this.#game_manager.player.order) return;
+			this.#game_manager.current_section.mode = GameSection.MODE_PUT;
+			console.log(this.#game_manager.current_section.mode);
+		});
+		this.#ingame_buttons.children[1].addEventListener('click', () => {
+			if (this.#game_manager.current_turn != this.#game_manager.player.order || this.#game_manager.checkTable(this.#game_manager.player.order)) return;
+			this.#game_manager.dispatchEvent(new Event.PutPassEvent(this.#game_manager.player.order));
+			document.getElementById('pass_button').classList.add('disabled');
+		});
+		this.#ingame_buttons.children[2].addEventListener('click', () => {
+			if (this.#game_manager.current_turn != this.#game_manager.player.order) return;
+			this.#game_manager.current_section.mode = GameSection.MODE_PUT;
+			// this.#current_section.mode = GameSection.MODE_BANG;
+		});
+
+		/*
+		 * ResultSection
+		 */
+		this.#restart_button.addEventListener('click', (e) => {
+			console.log(e);
+			this.#game_manager.dispatchEvent(new Event.GameRestartEvent());
+		});
+	}
+
+	show(dom, is_flex = false) {
+		if (is_flex) {
+			dom.style.display = "flex";
+		} else {
+			dom.style.display = "block";
+		}
+	}
+
+	order_update(){
+		let p = document.getElementById('order');
+		console.log(p)
+		if (this.#game_manager.current_turn == Disk.BLACK) {
+			p.children[0].innerText = '黒';
+			p.classList.remove('order-white');
+			p.classList.add('order-black');
+		} else {
+			p.children[0].innerText = '白';
+			p.classList.remove('order-black');
+			p.classList.add('order-white');
+		}
+	}
+
+	hide(dom) {
+		dom.style.display = "none";
 	}
 
 
