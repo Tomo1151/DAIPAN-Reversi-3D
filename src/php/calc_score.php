6fb018f84201e79beed955c37cbcccd370ce4459
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-03-01 22:18:47 +0900

    Delete calc_score.php

--- src/php/calc_score.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	if (!isset($_POST["token"]) || $_POST["token"] !== $_SESSION["token"]) {
-		die(json_encode("無効なセッション"));
-	} else {
-		$id = session_id();
-		echo json_encode("有効なセッション");
-	}
-?>
